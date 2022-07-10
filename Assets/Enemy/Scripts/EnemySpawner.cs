bdc8753dabf3e172a3508547192ed03d9d788f10
Maks <krivorukov2007@mail.ru>
2022-07-11 01:11:29 +0300

    Improved Enemy Spawner

--- Assets/Enemy/Scripts/EnemySpawner.cs
+++ Assets/Enemy/Scripts/EnemySpawner.cs
@@ -7,43 +7,18 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private GameObject enemyPrefub;
     public float intervalSpawn;
-    public Transform point;
-
-    private int counter = 0;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(spawnerEnemy(intervalSpawn, enemyPrefub));        
+        StartCoroutine(Spawner());        
     }
-
-    private void FixedUpdate()
-    {
-        enemySpawner2();
-    }
-
-    /*IEnumerator enemySpawner(float interval, GameObject enemy) Доработать    {
-        rot = new Quaternion(0, 0, 180, 0); // x,y,z,w
-        Instantiate(enemy, point.position, rot);
-
-        yield return new WaitForSecondsRealtime(interval);
-    }
-    */
-
-    private void enemySpawner2 ()
-    {
-        Quaternion rot = new Quaternion(0, 0, 180, 0); // x,y,z,w;
-        counter += 1;
-        
-
-        if (counter >= intervalSpawn)
+    IEnumerator Spawner()    {
+        while (true)
         {
-            counter = 0;
-            Instantiate(enemyPrefub, point.position, rot);
+            Instantiate(enemyPrefub, transform.position, transform.rotation);
+
+            yield return new WaitForSecondsRealtime(intervalSpawn);
         }
     }
-
-
 }
