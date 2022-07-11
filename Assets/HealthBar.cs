f350ff56e8fff14eac91b35e7b49e12ba9f2a939
Maks <krivorukov2007@mail.ru>
2022-07-11 21:28:52 +0300

    Created UI HP bar

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class HealthBar : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    private MainPlayer player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<MainPlayer>();
+        textMeshPro.text = (player.health).ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player) return;   
+        textMeshPro.text = (player.health).ToString();
+    }
+}
