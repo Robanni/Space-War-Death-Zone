5bd02b4ace3847ecd4f5783993f0266935708515
Maks <krivorukov2007@mail.ru>
2022-07-18 01:45:02 +0300

    Created Shop Window, and transition between windows

--- Assets/UI Toolkit/UIController.cs
+++ Assets/UI Toolkit/UIController.cs
@@ -8,19 +8,52 @@ public class UIController : MonoBehaviour
 {
 
     private Button buttonStart;
+    private Button buttonShop;
+    private Button buttonBack;
+    private Button buttonExitGame;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         var root = GetComponent<UIDocument>().rootVisualElement; // Search a root element of window
-        buttonStart = root.Q<Button>("start-button"); // Get a button on UXML document
 
-        buttonStart.clicked += StartButtonPressed; // Add a func in button
+        // Get refs of Windows
+        var mainWin = root.Q<VisualElement>("MainWin");
+        var shopWin = root.Q<VisualElement>("ShopWin");
+
+        // // Get refs of buttons
+        buttonStart = root.Q<Button>("start-button"); // Get a button on UXML document
+        buttonShop = root.Q<Button>("shop-button");
+        buttonBack = root.Q<Button>("back-button");
+        buttonExitGame = root.Q<Button>("exit-button");
+
+        // Add events on buttons
+        buttonStart.clicked += StartButtonPressed; 
+
+        buttonShop.clicked += () =>
+        {
+            
+            mainWin.AddToClassList("hide");
+            shopWin.RemoveFromClassList("hide");
+        };
+
+        buttonBack.clicked += () =>
+        {
+            mainWin.RemoveFromClassList("hide");
+            shopWin.AddToClassList("hide");
+        };
+
+        buttonExitGame.clicked += ExitButtonPressed;
     }
 
-    void StartButtonPressed()
+    private void StartButtonPressed()
     {
         SceneManager.LoadScene("Level_1"); // Loading scene of game
+    }
+
+    private void ExitButtonPressed()
+    {
+        Application.Quit();
     }
 
 }
